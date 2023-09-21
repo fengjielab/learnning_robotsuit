3321bab0867218b752dffd3ccfb16f514b1cf368
PearlLHF <hlyuag@connect.ust.hk>
2023-09-21 09:27:10 +0800

    object

--- robosuite/environments/manipulation/can.py
+++ robosuite/environments/manipulation/can.py
@@ -529,6 +529,8 @@ class PickPlaceCans(SingleArmEnv):
 
         for obj_cls, obj_name in zip(
             (CanObject, CanObject, CanObject, CanObject, CanObject, CanObject),
+            # ["Milk", "Bread", "Cereal", "Can", "Lemon", "Bottle"]
+            # (MilkObject, BreadObject, CerealObject, CanObject, LemonObject, BottleObject),
             self.obj_names,
         ):
             obj = obj_cls(name=obj_name)
